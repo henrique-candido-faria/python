dceed77fb918e51f2c1aab652d3fb4a8d4399666
henrique_faria <henrique.faria@socialminer.com>
2021-06-08 11:05:40 -0300

    add hello-world.py for tenting a build

---
+++ commands/operação.py
@@ -0,0 +1,73 @@
+# primeiro=int(input('Digite o primeiro número: '))
+# segundo=int(input('Digite o segundo número: '))
+# terceiro=input('Digite o terceiro número: ')
+# soma=primeiro+segundo
+# print(type(primeiro))
+# print(type(segundo))
+# print(type(terceiro))
+# print('A soma do primeiro e segundo número é:', soma)
+# print('A soma do primeiro e segundo número é: {}'.format(soma))
+# print('A soma entre', primeiro, 'e', segundo, 'vale: {}'.format(soma))
+# print('A soma entre {} e {} vale: {}'.format(primeiro, segundo, soma))
+
+# types: int, flooat, bool and str
+
+# Ordem de Precedência
+## () 
+## **
+## * / // %
+## + -
+
+# numero_inteiro=int(input('Digite um número inteiro: '))
+# sucessor=numero_inteiro+1
+# antecessor=numero_inteiro-1
+# print('Número antecessor {} ao {} e sucessor {}'.format(antecessor, numero_inteiro, sucessor))
+
+# numero=int(input('Digite um número: '))
+# dobro=numero*2
+# triplo=numero*3
+# raiz=numero**(1/2)
+# print('Dobro: {} \nTriplo: {} \nRaiz quadrada: {}'.format(dobro, triplo, raiz))
+
+# nota1=float(input('Digite a primeira nota: '))
+# nota2=float(input('Digite a segunda nota: '))
+# soma=(nota1+nota2)/2
+# print('A média é {}'.format(soma))
+
+# metros=float(input('Digite um valor em metros: '))
+# milimetros=metros*1000
+# centimetros=metros*100
+# print('Metros: {} Milimetros: {} Centimetros: {}'.format(metros, milimetros, centimetros))
+
+# tabuada=int(input('Digite um número: '))
+# a=tabuada*1
+# b=tabuada*2
+# c=tabuada*3
+# d=tabuada*4
+# e=tabuada*5
+# f=tabuada*6
+# g=tabuada*7
+# h=tabuada*8
+# i=tabuada*9
+# j=tabuada*10
+# print('A tabuada do número {} é:'.format(tabuada))
+# print(a, b, c, d, e, f, g, h, i, j)
+
+# dinheiro=float(input('Digite quanto você tem de dinheiro: '))
+# real=3.27
+# calculo=dinheiro/real
+# print('Você pode comprar ${}} dolares'.format(calculo))
+
+# largura=float(input('Digite a largura da parede: '))
+# altura=float(input('Digite a altura da parede: '))
+# area=largura*altura
+# calculo=area/2
+# print('A quantidade de tinta a ser gasta é: {}'.format(calculo))
+
+# preço=float(input('Digite o valor do produto: '))
+# desconto=(preço*5)/100
+# print('O desconto para o produto de valor {} será de: R${}'.format(preço, desconto))
+
+# salario=float(input('Digite o valor do salario: '))
+# aumento=(salario*15)/100
+# print('O aumento do salario será de: R${}'.format(aumento))

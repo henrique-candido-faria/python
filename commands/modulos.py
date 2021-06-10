167265e8fc26c3d26e9d1d28d727c540a353d3f1
henrique_faria <henrique.faria@socialminer.com>
2021-06-10 11:42:25 -0300

    building python

---
+++ commands/modulos.py
@@ -0,0 +1,25 @@
+# import math
+# from random import random
+# numero = int(input('Digite um número: '))
+# raiz = math.sqrt(numero)
+# print('A raiz de {} é igual a {}'.format(numero, raiz))
+# print('A raiz de {} é igual arredondando para cima {}'.format(numero, math.ceil(raiz)))
+# print('A raiz de {} é igual arredondando para baixo {}'.format(numero, math.floor(raiz)))
+
+# from math import sqrt, ceil, floor
+# numero = int(input('Digite um número: '))
+# raiz = sqrt(numero)
+# print('A raiz de {} é igual a {}'.format(numero, raiz))
+# print('A raiz de {} é igual arredondando para cima {}'.format(numero, ceil(raiz)))
+# print('A raiz de {} é igual arredondando para baixo {}'.format(numero, floor(raiz)))
+
+# import random
+# numero = random.random()
+# print(numero)
+
+# import random
+# numero = random.randint(1, 10)
+# print(numero)
+
+import emoji
+print(emoji.emojize("Helo Wold :earth_americas:", use_aliases=True))
